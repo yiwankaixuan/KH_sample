6fc80cf87fc4dc1b2aeaebed28af1f0511d2512f
wankai <yiwankaixuan@gmail.com>
2017-11-09 08:22:30 +0000

    Finish static pages

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 class StaticPagesController extends Controller
 {
   public function home()
-  {
-      return view('static_pages/home');
-  }
+{
+  return view('static_pages/home');
+}
 
-  public function help()
-  {
-      return view('static_pages/help');
-  }
+public function help()
+{
+  return view('static_pages/help');
+}
 
-  public function about()
-  {
-      return view('static_pages/about');
-  }
+public function about()
+{
+  return view('static_pages/about');
+}
 }
