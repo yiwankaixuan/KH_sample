75e7d0f72e450dd9ca7c695fc985b6d6fc97d4f4
wankai <yiwankaixuan@gmail.com>
2017-11-14 07:34:16 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -10,16 +10,17 @@ class UsersTableSeeder extends Seeder
      *
      * @return void
      */
-     public function run()
-     {
-         $users = factory(User::class)->times(50)->make();
-         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
-         $user = User::find(1);
-          $user->name = 'Aufree';
-          $user->email = 'aufree@yousails.com';
-          $user->password = bcrypt('password');
-          $user->is_admin = true;
-          $user->save();
-     }
+        $user = User::find(1);
+        $user->name = 'Aufree';
+        $user->email = 'aufree@yousails.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->activated = true;
+        $user->save();
+    }
 }
