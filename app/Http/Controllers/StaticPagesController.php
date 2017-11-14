cece5925ef3f28ca6718e0d61912e557ed52e564
wankai <yiwankaixuan@gmail.com>
2017-11-14 10:21:36 +0000

    Add user statuses

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -4,20 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Models\Status;
+use Auth;
+
 class StaticPagesController extends Controller
 {
-  public function home()
-{
-  return view('static_pages/home');
-}
 
-public function help()
-{
-  return view('static_pages/help');
-}
+    public function home()
+    {
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
 
-public function about()
-{
-  return view('static_pages/about');
-}
+        return view('static_pages/home', compact('feed_items'));
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
 }
